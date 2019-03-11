1f05be4e57281f4fa2e1a9cf7f27fb45385be853
Antonio Fabregat <fabregat.antonio@gmail.com>
2019-03-11 16:24:22 +0000

    score default set to 0.75

--- src/main/java/org/reactome/server/pathways/xtalk/Main.java
+++ src/main/java/org/reactome/server/pathways/xtalk/Main.java
@@ -31,9 +31,9 @@ public class Main {
                         new FlaggedOption("host",     JSAP.STRING_PARSER, "localhost", JSAP.NOT_REQUIRED, 'h', "host",     "The neo4j host"),
                         new FlaggedOption("port",     JSAP.STRING_PARSER, "7474",      JSAP.NOT_REQUIRED, 'p', "port",     "The neo4j port"),
                         new FlaggedOption("user",     JSAP.STRING_PARSER, "neo4j",     JSAP.NOT_REQUIRED, 'u', "user",     "The neo4j user"),
-                        new FlaggedOption("score",    JSAP.STRING_PARSER, "0.45",      JSAP.NOT_REQUIRED, 's', "score",    "Threshold score for IntAct interactions (range [0.45, 1])"),
-                        new FlaggedOption("output",   JSAP.STRING_PARSER,  null,       JSAP.REQUIRED,     'o', "output",   "Output file"),
-                        new FlaggedOption("password", JSAP.STRING_PARSER, "neo4j",     JSAP.REQUIRED,     'k', "password", "The neo4j password")
+                        new FlaggedOption("password", JSAP.STRING_PARSER, "neo4j",     JSAP.REQUIRED,     'k', "password", "The neo4j password"),
+                        new FlaggedOption("score",    JSAP.STRING_PARSER, "0.75",      JSAP.NOT_REQUIRED, 's', "score",    "Threshold score for IntAct interactions (range [0.45, 1]). Default 0.75"),
+                        new FlaggedOption("output",   JSAP.STRING_PARSER,  null,       JSAP.REQUIRED,     'o', "output",   "Output file")
                 }
         );
 
@@ -98,7 +98,7 @@ public class Main {
     }
 
     private static double getScore(String score) {
-        double rtn = 0.45;
+        double rtn = 0.75;
         try {
             rtn = Double.valueOf(score);
         } catch (NumberFormatException e) {
